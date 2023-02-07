ae67cdc5ee55be6837c9a0d454bc538375717795
Ankit <ankitojha1409@gmail.com>
2023-02-08 01:33:43 +0530

    lambda function in python

---
+++ 100daysOfCode/lambdaFun.py
@@ -0,0 +1,9 @@
+sum = lambda num1, num2 : num2+num1
+sub = lambda num1, num2 : num2-num1
+div = lambda num1, num2 : num2/num1
+mult = lambda num1, num2 : num2*num1
+
+print("the sum of 5 & 7 is : ",sum(5,7))
+print("the sub of 5 & 7 is : ",sub(5,7))
+print("the mult of 5 & 7 is : ",mult(5,7))
+print("the div of 5 & 7 is : ",div(5,7))
