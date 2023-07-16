a30d908e836c5bb6194e2fa50cbfa70f3c5efaaf
Ankit Ojha <ankitojha1409@gmail.com>
2023-07-16 18:36:09 +0530

    codeforces A 800 Word capitalization done

--- 100daysOfCode/file-system/filesys.py
+++ 100daysOfCode/file-system/filesys.py
@@ -1,16 +1,28 @@
-file = open("file.txt")
+# file = open("file.txt")
 
 # file.write('I am writing content in file!!')
 
 # file.write('\nNow I am tring to append the content inside the file !!')
 
-s = file.read()
-print(s)
+# s = file.read()
+# print(s)
 
-file.close()
+# file.close()
 
 
 
 
 #below line will show error because the file is closed now
 # file.write('x')
+
+
+s = "Hey there I am a programmer"
+
+with open("test.txt","w") as f :
+    f.write(s)
+    f.close()
+
+
+with open("test.txt","r") as f :
+    r = f.read()
+    print(r)
