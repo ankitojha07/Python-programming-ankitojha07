f595c4ec4ce90e94bcca94db74dd5ec44873f4cb
Ankit Ojha <ankitojha1409@gmail.com>
2023-11-21 01:15:35 +0530

    Time module in python

---
+++ 100daysOfCode/date-and-time/goodMorning.py
@@ -0,0 +1,13 @@
+import time
+
+timeStamp = time.strftime('%H:%M:%S')
+print(timeStamp)
+
+timeStamp = time.strftime('%H')
+print(timeStamp)
+
+timeStamp = time.strftime('%M')
+print(timeStamp)
+
+timeStamp = time.strftime('%S')
+print(timeStamp)
